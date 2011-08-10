1237d495edb2536f1224f9c99aeef4dc5fa93606
ravibalgi <ravibalgi@gmail.com>
2011-08-11 00:10:29 +0530

    configuration file for scraper

---
+++ nregadbconfig.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+#Database configuration for Ashwin's server
+host="mysql.enrega.dreamhosters.com"
+user="enrega"
+passcode="hBBkaCwz"
+database="enrega"
+
+#table configuration
+district_expense="rega_enrega_districtexpenses"
+taluka_expense="rega_enrega_blockexpenses"
+panchayat_expense="rega_enrega_grampanchayatexpenses"
+
+#district page link year wise
+districtyear1112="http://164.100.112.66/netnrega/writereaddata/citizen"\
+                            "_out/phy_fin_reptemp_Out_18_local_1112.html"
+districtyear1011="http://164.100.112.66/netnrega/writereaddata/citizen"\
+                            "_out/phy_fin_reptemp_Out_18_local_1011.html"
